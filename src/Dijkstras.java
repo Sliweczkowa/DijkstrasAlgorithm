6c6b116446fed311046936a199544843437579f9
sliweczkowa <w.sliwinska@outlook.com>
2024-04-02 10:49:18 +0200

    fix: cities initialisation; feat: BiMap

--- src/Dijkstras.java
+++ src/Dijkstras.java
@@ -1,21 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 public class Dijkstras {
     public static Integer dijkstars(Input input) {
         List<CityInfo> cityInfo = new ArrayList<>();
 
-        // First city initialisation
-        cityInfo.add(new CityInfo(input.getStartingPoint()));
-        cityInfo.get(0).setMinDistance(0d);
-        cityInfo.get(0).setVisited(true);
-
-        // Remaining cities initialisation
-        int iterator = input.getCitiesNumber() - 1;
-        for (int i = iterator; i < input.getConnections().size(); i += iterator) {
-            cityInfo.add(new CityInfo(input.getConnections().get(i).getCity1()));
-            iterator--;
+        // Cities initialisation
+        for (Connection connection : input.getConnections()) {
+            boolean isPresentCity1 = false;
+            boolean isPresentCity2 = false;
+            for (CityInfo connectionCityInfo : cityInfo) {
+                if (connection.getCity1().equals(connectionCityInfo.getCityName())) {
+                    isPresentCity1 = true;
+                }
+                if (connection.getCity2().equals(connectionCityInfo.getCityName())) {
+                    isPresentCity2 = true;
+                }
+            }
+            if (!isPresentCity1) {
+                cityInfo.add(new CityInfo(connection.getCity1()));
+            }
+            if (!isPresentCity2) {
+                cityInfo.add(new CityInfo(connection.getCity2()));
+            }
         }
+
+        // CityId - CityName bidirectional map
+        BiMap<Integer, String> cities = HashBiMap.create();
+        for (int i = 0; i < cityInfo.size(); i++) {
+            cities.put(i, cityInfo.get(i).getCityName());
+        }
+
+        // First city as starting point
+        cityInfo.get(cities.inverse().get(input.getStartingPoint())).setMinDistance(0d);
+        cityInfo.get(cities.inverse().get(input.getStartingPoint())).setVisited(true);
 
         List<String> currentlyConnectedCities = new ArrayList<>();
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Input input = new Input("res/test");
+        Integer shortestDistance = Dijkstras.dijkstars(input);
     }
 }
